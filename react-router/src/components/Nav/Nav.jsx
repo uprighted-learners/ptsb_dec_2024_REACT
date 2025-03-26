24a06f2ca721b48e42c2551b4404d8293fee6ac2
u4ik <insighteuphoric@gmail.com>
2025-03-26 19:50:49 -0400

    updated navlink so active-link works correctly

--- react-router/src/components/Nav/Nav.jsx
+++ react-router/src/components/Nav/Nav.jsx
@@ -5,9 +5,9 @@ import { NavLink } from "react-router-dom"
 const Nav = () => {
   return (
     <nav>
-        <NavLink to="/" className={(isActive) => (isActive ? 'active-link' : "") }>Home</NavLink>
-        <NavLink to="/about" className={(isActive) => (isActive ? 'active-link' : "") } >About</NavLink>
-        <NavLink to="/projects" className={(isActive) => (isActive ? 'active-link' : "") }>Projects</NavLink>
+        <NavLink to="/" className={({isActive}) => (isActive ? 'active-link' : "") }>Home</NavLink>
+        <NavLink to="/about" className={({isActive}) => (isActive ? 'active-link' : "") } >About</NavLink>
+        <NavLink to="/projects" className={({isActive}) => (isActive ? 'active-link' : "") }>Projects</NavLink>
     </nav>
   )
 }
